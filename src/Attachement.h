0a1f0e3490fe486e9290ea23bfd205c6e81edf32
Misael Braga de Bitencourt <misael.bitencourt@videosoft.com.br>
2022-08-23 18:44:01 -0300

    attachement base64 convertion

--- src/Attachement.h
+++ src/Attachement.h
@@ -10,7 +10,23 @@ class Attachement {
 
         void base64Encode()
         {
-            // TODO
+            std::fstream attachementFile(fileName.c_str());
+            if (attachementFile.is_open())
+            {
+                attachementFile.seekg(0, attachementFile.end);
+                unsigned int length = attachementFile.tellg();
+                attachementFile.seekg(0, attachementFile.beg);
+                char * buffer = new char[length];
+                attachementFile.read(buffer, length);
+                attachementFile.close();
+                base64 = base64_encode((unsigned char const*)buffer, length, false);
+                delete []buffer;
+            }
+            else
+            {
+                printf("Failed to open the file %s", fileName.c_str());
+                exit(1);
+            }
         }
 
 };
